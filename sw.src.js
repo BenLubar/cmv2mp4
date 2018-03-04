d471b53f7ab5e1bbef8ea293236b11a56dc4f7f9
Ben Lubar <ben.lubar@gmail.com>
2018-03-04 12:03:34 -0600

    Work around GoogleChrome/workbox#1344

--- sw.src.js
+++ sw.src.js
@@ -1,5 +1,13 @@
+var realDBOpen = indexedDB.open;
+indexedDB.open = function(name) {
+	if (name === 'workbox-precaching') {
+		return realDBOpen.call(indexedDB, 'workbox-precaching-cmv2mp4');
+	}
+	return realDBOpen.apply(indexedDB, arguments);
+};
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js');
-workbox.core.setCacheNameDetails({prefix: 'cmv2mp4-'});
+
 workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.googleAnalytics.initialize();
